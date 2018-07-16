63000b079af5ad9bba35150eea2a9a57235f2eb9
Suman <suman@example.com>
2018-07-16 13:46:33 +0800

    add new js library

---
+++ ForexAppClient/ForexApp.js
@@ -0,0 +1,26 @@
+// JS library for ForexApp
+
+var ForexApp = {
+    // Variables
+    Assest = null,
+    Transaction = null,
+    SetDataModel : function(){
+        // Create a data model to be used in application
+    },
+    GetDataModel : function() {
+        // return Data model that are being used in application
+    },
+    GetAllTransactions : function(){
+        // Get all transactions from server
+        var result = null;
+        return result;
+    },
+    CreateTransaction : function (args){
+        // Send args to the server to create transactions
+    },
+    UpdateTransaction : function (args){
+        // Send args to the server to update transactions
+    }
+
+
+}
